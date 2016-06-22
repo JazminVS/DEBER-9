1ba778cf8e06081685e7aff6538254be3824be31
JazminVS <jazminv8.95@gmail.com>
2016-06-22 14:54:07 -0500

    Version Final
    Se realiza cambios en colores y apariencia y se modifica para cuando se
    muevan por medio de teclas solo lo hagan las especificadas,
    anteriormente.

--- Poligonos.py
+++ Poligonos.py
@@ -1,21 +1,26 @@
-import time
 from tkinter import *
-import turtle
 tk=Tk()
-canvas=Canvas(tk, width=500, height=500)
+tk.title('POLIGONOS EN MOVIMIENTO')
+canvas=Canvas(tk, width=500, height=500,bg="orange")
 canvas.pack(expand=YES, fill=BOTH)
-canvas.create_rectangle ((10, 10, 40, 40), fill="blue")
-canvas.create_arc(10, 10, 70, 70, start=0, extent=90, fill="pink")
-canvas.create_oval(10, 10, 50, 50, width=2, fill="purple")
+canvas.create_rectangle((10, 10, 40, 40),width=2, fill="white")
+canvas.create_arc(10, 10, 70, 70, start=0, extent=90, width=2,fill="pink")
+canvas.create_oval(10, 10, 50, 50, width=2, fill="blue")
 def movefig1(event):
 	if event.keysym=='Up':
-		canvas.move(3,0,-3)
+		canvas.move(1,0,-3)
+		canvas.move(2,0,-3)
 	elif event.keysym=='Down':
 		canvas.move(1,0,3)
+		canvas.move(3,0,3)
+		canvas.move(2,0,3)
 	elif event.keysym=='Left':
-		canvas.move(2,-3,0)
+		canvas.move(1,-3,0)
+		canvas.move(3,-3,0)
 	elif event.keysym=='Right':
+		canvas.move(1,3,0)
 		canvas.move(2,3,0)
+		canvas.move(3,3,0)
 
 canvas.bind_all('<KeyPress-Up>', movefig1)
 canvas.bind_all('<KeyPress-Down>', movefig1)
