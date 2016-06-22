9d8b103c14e83a35ee5d069f0259d88d86c30c4f
JazminVS <jazminv8.95@gmail.com>
2016-06-22 14:52:43 -0500

    Cambios en el programa estrella
    Estas condiciones nos permiten el buen funcionamiento restringue el
    dibujo de estrellas pares o menores de 5 puntas.

--- Estrella.py
+++ Estrella.py
@@ -1,15 +1,22 @@
 #REALIZAR UNA ESTRELLA DE N LADOS IMPARES
 a=int(input("INGRESE EL NUMERO DE PUNTAS DE SU ESTRELLA: "))
 import turtle 
-t=turtle.Pen()
 
-#if(a==0 or a==1 or a==2 or a==3 or a==4):
-	#print("LO SIENTO! NO ENTIENDO TU ORDEN >_<")
-if (a%2 !=0):
-	angulo=180/a
-	for x in range(1,a+1):
-		t.forward(-100)
-		t.left(180-angulo)
-	turtle.getscreen()._root.mainloop()
-else:
+if(a<=4):
 	print("LO SIENTO! NO ENTIENDO TU ORDEN >_<")
+else:
+	if (a%2!=0 ):
+		t=turtle.Pen()
+		ventana=turtle.Screen()
+		ventana.bgcolor("orange")
+		t.color(0,0,0)
+		t.begin_fill()
+		angulo=180/a
+		for x in range(1,a+1):
+			t.forward(-150)
+			t.left(180-angulo)
+		t.end_fill()
+		turtle.getscreen()._root.mainloop()
+	else:
+		print("LO SIENTO! NO ENTIENDO TU ORDEN >_<")
+
